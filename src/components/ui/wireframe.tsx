05f8ccace01628c112980298b2579c20c1f0aaf4
Diego Ramos <rfdiego@icloud.com>
2025-11-29 12:18:07 -0400

    control sidebar side

--- src/components/ui/wireframe.tsx
+++ src/components/ui/wireframe.tsx
@@ -9,31 +9,43 @@ function Wireframe({ className, ...props }: React.ComponentProps<"div">) {
 				// WILL BE POSITIONED OUTSIDE OF THIS AREA AS FIXED ELEMENTS
 				"has-data-wf-nav:mt-16",
 				"has-data-wf-footer:mb-16",
-				"has-data-[wf-sidebar=expanded]:ml-52",
-				"has-data-[wf-sidebar=collapsed]:ml-16",
+				"has-data-[wf-left-sidebar=expanded]:ml-52",
+				"has-data-[wf-right-sidebar=expanded]:mr-52",
+				"has-data-[wf-left-sidebar=collapsed]:ml-16",
+				"has-data-[wf-right-sidebar=collapsed]:mr-16",
 
 				// RESPONSIVE NAVBAR HEIGHT AND MARGINS
 				"**:data-wf-responsive-nav:h-16",
 				"has-data-wf-responsive-nav:mb-16",
 				"md:has-data-wf-responsive-nav:mt-16 md:has-data-wf-responsive-nav:mb-0",
 				// ADD SIDEBAR MARGIN TO RESPONSIVE NAV IF SIDEBAR EXISTS
-				"has-data-wf-responsive-nav:has-data-[wf-sidebar=expanded]:**:data-wf-responsive-nav:ml-52",
-				"has-data-wf-responsive-nav:has-data-[wf-sidebar=collapsed]:**:data-wf-responsive-nav:ml-16",
+				"has-data-wf-responsive-nav:has-data-[wf-left-sidebar=expanded]:**:data-wf-responsive-nav:ml-52",
+				"has-data-wf-responsive-nav:has-data-[wf-right-sidebar=expanded]:**:data-wf-responsive-nav:mr-52",
+				"has-data-wf-responsive-nav:has-data-[wf-left-sidebar=collapsed]:**:data-wf-responsive-nav:ml-16",
+				"has-data-wf-responsive-nav:has-data-[wf-right-sidebar=collapsed]:**:data-wf-responsive-nav:mr-16",
 
 				// PASS DOWN THE DIMENSIONS FOR THE NAV, FOOTER AND SIDEBAR
 				// TOP NAVIGATION HEIGHT
 				"**:data-wf-nav:h-16",
 				// ADD SIDEBAR MARGIN TO TOP NAV IF SIDEBAR EXISTS
-				"has-data-wf-nav:has-data-[wf-sidebar=expanded]:**:data-wf-nav:ml-52",
-				"has-data-wf-nav:has-data-[wf-sidebar=collapsed]:**:data-wf-nav:ml-16",
+				"has-data-wf-nav:has-data-[wf-left-sidebar=expanded]:**:data-wf-nav:ml-52",
+				"has-data-wf-nav:has-data-[wf-right-sidebar=expanded]:**:data-wf-nav:mr-52",
+				"has-data-wf-nav:has-data-[wf-left-sidebar=collapsed]:**:data-wf-nav:ml-16",
+				"has-data-wf-nav:has-data-[wf-right-sidebar=collapsed]:**:data-wf-nav:mr-16",
 				// BOTTOM FOOTER HEIGHT
 				"**:data-wf-footer:h-16",
 				// ADD SIDEBAR MARGIN TO FOOTER IF SIDEBAR EXISTS
-				"has-data-wf-footer:has-data-[wf-sidebar=expanded]:**:data-wf-footer:ml-52",
-				"has-data-wf-footer:has-data-[wf-sidebar=collapsed]:**:data-wf-footer:ml-16",
-				// SIDEBAR WIDTH
-				"**:data-[wf-sidebar=expanded]:w-52",
-				"**:data-[wf-sidebar=collapsed]:w-16",
+				"has-data-wf-footer:has-data-[wf-left-sidebar=expanded]:**:data-wf-footer:ml-52",
+				"has-data-wf-footer:has-data-[wf-right-sidebar=expanded]:**:data-wf-footer:mr-52",
+				"has-data-wf-footer:has-data-[wf-left-sidebar=collapsed]:**:data-wf-footer:ml-16",
+				"has-data-wf-footer:has-data-[wf-right-sidebar=collapsed]:**:data-wf-footer:mr-16",
+				// SIDEBAR WIDTH AND POSITIONING
+				"**:data-wf-left-sidebar:left-0",
+				"**:data-wf-right-sidebar:right-0",
+				"**:data-[wf-left-sidebar=expanded]:w-52",
+				"**:data-[wf-right-sidebar=expanded]:w-52",
+				"**:data-[wf-left-sidebar=collapsed]:w-16",
+				"**:data-[wf-right-sidebar=collapsed]:w-16",
 				className,
 			)}
 			{...props}
@@ -92,20 +104,28 @@ function WireframeResponsiveNav({
 	);
 }
 
+type WireframeCollapsableSidebarSide = "left" | "right";
+
 function WireframeCollapsableSidebar({
 	className,
 	children,
 	collapsed = false,
+	side = "left",
 	...props
-}: React.ComponentProps<"div"> & { collapsed?: boolean }) {
+}: React.ComponentProps<"div"> & {
+	collapsed?: boolean;
+	side?: WireframeCollapsableSidebarSide;
+}) {
 	return (
 		<div
 			className={cn(
-				"fixed inset-y-0 left-0 z-50 overflow-scroll [scrollbar-width:none] [&::-webkit-scrollbar]:hidden",
+				"fixed inset-y-0 z-50 overflow-scroll [scrollbar-width:none] [&::-webkit-scrollbar]:hidden",
 				className,
 			)}
-			data-wf-sidebar={collapsed ? "collapsed" : "expanded"}
-			{...props}
+			{...{
+				...props,
+				[`data-wf-${side}-sidebar`]: `${collapsed ? "collapsed" : "expanded"}`,
+			}}
 		>
 			{children}
 		</div>
@@ -118,4 +138,5 @@ export {
 	WireframeFooter,
 	WireframeResponsiveNav,
 	WireframeCollapsableSidebar,
+	type WireframeCollapsableSidebarSide,
 };
--- src/components/wireframe/sidebar.tsx
+++ src/components/wireframe/sidebar.tsx
@@ -1,21 +1,36 @@
 "use client";
 
 import { useState } from "react";
-import { WireframeCollapsableSidebar } from "@/components/ui/wireframe";
+import {
+	WireframeCollapsableSidebar,
+	type WireframeCollapsableSidebarSide,
+} from "@/components/ui/wireframe";
 
 export function Sidebar() {
 	const [collapsed, setCollapsed] = useState(false);
+	const [side, setSide] = useState<WireframeCollapsableSidebarSide>("left");
 
 	function handleClick() {
 		setCollapsed(!collapsed);
 	}
 
 	return (
-		<WireframeCollapsableSidebar collapsed={collapsed}>
+		<WireframeCollapsableSidebar collapsed={collapsed} side={side}>
 			<div className="flex min-h-full w-full items-center justify-between bg-pink-500/40">
-				<button onClick={handleClick} type="button">
-					Click me
-				</button>
+				<div className="flex flex-col gap-4">
+					<button onClick={handleClick} type="button">
+						Click me
+					</button>
+					<select
+						onChange={(e) =>
+							setSide(e.target.value as WireframeCollapsableSidebarSide)
+						}
+						value={side}
+					>
+						<option value="left">Left</option>
+						<option value="right">Right</option>
+					</select>
+				</div>
 				{/* <div className="flex flex-col">
 					<div className="h-[800px] w-4 bg-red-600">a</div>
 					<div className="h-[500px] w-4 bg-blue-600">b</div>
