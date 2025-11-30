6d50cda86b374329a201c97c90d1725075c6e805
Diego Ramos <rfdiego@icloud.com>
2025-11-30 17:20:13 -0400

    move vars to component to allow multiple wireframes

--- src/components/ui/wireframe.tsx
+++ src/components/ui/wireframe.tsx
@@ -54,6 +54,20 @@ function Wireframe({
 				// HACK TO FIX CHILDREN HEIGHT 100% ISSUE
 				"relative",
 			)}
+			style={
+				{
+					"--sticky-nav-height": "calc(var(--spacing) * 12)",
+					"--sticky-nav-offset": "calc(var(--spacing) * 0)",
+					"--top-nav-height": "calc(var(--spacing) * 16)",
+					"--top-nav-offset": "calc(var(--spacing) * 0)",
+					"--bottom-nav-height": "calc(var(--spacing) * 8)",
+					"--bottom-nav-offset": "calc(var(--spacing) * 0)",
+					"--left-sidebar-width-collapsed": "calc(var(--spacing) * 16)",
+					"--right-sidebar-width-collapsed": "calc(var(--spacing) * 16)",
+					"--left-sidebar-width-expanded": "calc(var(--spacing) * 52)",
+					"--right-sidebar-width-expanded": "calc(var(--spacing) * 52)",
+				} as React.CSSProperties
+			}
 		>
 			<div
 				// INNER WRAPPER TO COVER THE WHOLE AREA
